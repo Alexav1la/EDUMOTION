5bf1478fd6f6f0292155d9f49bd2b5f2420f42cd
Alex Avila <0808alexavila@gmail.com>
2025-10-29 18:43:37 -0500

    Initial commit: proyecto Edumotion con configuración Vite y Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./app.css"
+
+function App() {
+
+  return (
+    <>
+      <div className="flex">
+        <h1 className="text-8xl">EDUMOTION</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
